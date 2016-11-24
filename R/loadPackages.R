4caa5198e86e02bb6604cf1b57b1a4ec95698036
בנימן הגלילי <benyominhagalili@mail.ru>
2016-11-24 21:21:20 +0200

    install if needed

--- R/loadPackages.R
+++ R/loadPackages.R
@@ -7,16 +7,27 @@
 #' loadWealthEffectPackages()
 loadWealthEffectPackages <- function(load=TRUE){
     if(load==TRUE){
+        install_if_needed(dplyr)
         library(dplyr)
+        install_if_needed(reshape2)
         library(reshape2)
+        install_if_needed(tidyr)
         library(tidyr)
+        install_if_needed(roxygen2)
         library(roxygen2)
+        install_if_needed(purrr)
         library(purrr)
+        install_if_needed(broom)
         library(broom)
+        install_if_needed(foreign)
         library(foreign)
+        install_if_needed(rJava)
         library(rJava)
+        install_if_needed(xlsx)
         library(xlsx)
+        install_if_needed(magrittr)
         library(magrittr)
+        install_if_needed(ggplot2)
         library(ggplot2)
 
     }
