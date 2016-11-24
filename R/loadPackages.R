5bba58c9b94bb429d7c6703922f28c72b9d7bb73
בנימן הגלילי <benyominhagalili@mail.ru>
2016-11-24 21:28:01 +0200

    added "quotes" to "packageNames"

--- R/loadPackages.R
+++ R/loadPackages.R
@@ -1,5 +1,6 @@
 #' Loads the required packages for the
 #' Housing Wealth Effect Analysis.
+#' Provides for simple dependency management to aid in reproducibility.
 #' @param load Defaults to TRUE.
 #' @keywords packages packageManagement dependencies library require requirements
 #' @export
@@ -7,27 +8,27 @@
 #' loadWealthEffectPackages()
 loadWealthEffectPackages <- function(load=TRUE){
     if(load==TRUE){
-        install_if_needed(dplyr)
+        install_if_needed("dplyr")
         library(dplyr)
-        install_if_needed(reshape2)
+        install_if_needed("reshape2")
         library(reshape2)
-        install_if_needed(tidyr)
+        install_if_needed("tidyr")
         library(tidyr)
-        install_if_needed(roxygen2)
+        install_if_needed("roxygen2")
         library(roxygen2)
-        install_if_needed(purrr)
+        install_if_needed("purrr")
         library(purrr)
-        install_if_needed(broom)
+        install_if_needed("broom")
         library(broom)
-        install_if_needed(foreign)
+        install_if_needed("foreign")
         library(foreign)
-        install_if_needed(rJava)
+        install_if_needed("rJava")
         library(rJava)
-        install_if_needed(xlsx)
+        install_if_needed("xlsx")
         library(xlsx)
-        install_if_needed(magrittr)
+        install_if_needed("magrittr")
         library(magrittr)
-        install_if_needed(ggplot2)
+        install_if_needed("ggplot2")
         library(ggplot2)
 
     }
