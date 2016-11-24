41053317e05520d2fd7c4d1fb15d0601196d71d5
בנימן הגלילי <benyominhagalili@mail.ru>
2016-11-24 17:34:48 +0200

    update version #

--- R/censusFunction1.R
+++ R/censusFunction1.R
@@ -1,11 +1,14 @@
 #' A Census Function
 #'
 #' This function allows you to do something.
-#' @param year
+#' @param year The year which you will perform the function on.
 #' @keywords individual, household
 #' @export
 #' @examples
 #' censusFunction1()
+#' censusFunction1(1976)
+#' censusFunction1(1977)
+#' censusFunction1(1978)
 censusFunction1 <- function(year=TRUE){
     if(year==1976){
         print("This is the census data for 1976.")
---
+++ R/censusFunction2.R
@@ -0,0 +1,35 @@
+#' A Census Function
+#'
+#' This function allows you to do something.
+#' @param year
+#' @keywords individual, household
+#' @export
+#' @examples
+#' censusFunction1()
+censusFunction1 <- function(year=TRUE){
+    if(year==1976){
+        print("This is the census data for 1976.")
+    }
+    else if(year==1977){
+        print("The year is 1977")
+      }
+    else {
+        print("Census data for those years not yet implemented.")
+    }
+}
+# assumes:
+#source("stats.r")
+#import data
+#source("importCensus.r")
+# #how many columns are there? = 74
+# ncol(file456)
+# #age
+# summary(file456$AGE)
+# #age is grouped by 0-4 years old, 5-9, 10-14, etc. until 85+
+# hist(file456$AGE)
+# #subset a few columns
+# subSetInd2014<-dplyr::select(file456,YEARSUR,WPLDIST,USSRBORN,INCSAL)
+# ##subset to only 3 cities, easier to copy DFW boxplot code
+# subCitiesInd2014 <- subSetInd2014 %>% dplyr::filter(WPLDIST=="Center"    |
+#                                                         WPLDIST=="Tel-Aviv"  |
+#                                                         WPLDIST=="Gush Dan")
