35ac8033cc3cccc3c9a9e48d7355db30b7133fe1
בנימן הגלילי <benyominhagalili@mail.ru>
2017-01-15 12:26:07 +0200

    add link to instructions

--- testFunctions.R
+++ testFunctions.R
@@ -1,10 +1,14 @@
+# Instructions to update package
+# https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
 library(roxygen2)
 library(devtools)
 devtools::install_github("hadley/tidyverse")
 library(tidyverse)
 setwd('~/censusFunctions')
 document()
-install('censusFunctions')
+#setwd("..")
+#install('censusFunctions')
+#create("matrices")
 censusFunction1(1978)
 censusFunction1(1976)
 censusFunction1(1977)
