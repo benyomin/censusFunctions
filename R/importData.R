1c760b2972ff430b397d8a3e8a290d394d0ddf01
בנימן הגלילי <benyominhagalili@mail.ru>
2017-01-16 20:13:14 +0200

    super assignment for local loading

--- R/importData.R
+++ R/importData.R
@@ -66,17 +66,17 @@ renameColumns() ## adds a year column
 
       ##  source("../includes/getProcessedData.R", echo=FALSE)
 
-exp2004s <-dget("../dataframes/exp2004s.txt")
-exp2005s <-dget("../dataframes/exp2005s.txt")
-exp2006s <-dget("../dataframes/exp2006s.txt")
-exp2007s <-dget("../dataframes/exp2007s.txt")
-exp2008s <-dget("../dataframes/exp2008s.txt")
-exp2009s <-dget("../dataframes/exp2009s.txt")
-exp2010s <-dget("../dataframes/exp2010s.txt")
-exp2011s <-dget("../dataframes/exp2011s.txt")
-exp2012s <-dget("../dataframes/exp2012s.txt")
-exp2013s <-dget("../dataframes/exp2013s.txt")
-exp2014s <-dget("../dataframes/exp2014s.txt")
+exp2004s <<-dget("../dataframes/exp2004s.txt")
+exp2005s <<-dget("../dataframes/exp2005s.txt")
+exp2006s <<-dget("../dataframes/exp2006s.txt")
+exp2007s <<-dget("../dataframes/exp2007s.txt")
+exp2008s <<-dget("../dataframes/exp2008s.txt")
+exp2009s <<-dget("../dataframes/exp2009s.txt")
+exp2010s <<-dget("../dataframes/exp2010s.txt")
+exp2011s <<-dget("../dataframes/exp2011s.txt")
+exp2012s <<-dget("../dataframes/exp2012s.txt")
+exp2013s <<-dget("../dataframes/exp2013s.txt")
+exp2014s <<-dget("../dataframes/exp2014s.txt")
 
       return("Imported selected portions of Housing Expenditure Survey. Returns exp2004-14s")
     }else if(source=="writeOut"){
