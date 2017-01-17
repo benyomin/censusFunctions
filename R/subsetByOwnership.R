ee55f4399ac9e92eedb309a4741eecf3ea471e67
בנימן הגלילי <benyominhagalili@mail.ru>
2017-01-17 09:20:44 +0200

    subsetByOwnership function, calls weighHouseholds(all)

---
+++ R/subsetByOwnership.R
@@ -0,0 +1,22 @@
+#' Creates dataframes expYYYYowners and expYYYYrenters, calles weighHouseholds.
+#'
+#' This function creates subsets of renters and owners.
+#' @param arg Defaults to true.
+#' @keywords owners, renters, subset
+#' @export
+#' @examples
+#' subsetByOwnership()
+#' subsetByOwnership(1)
+#' subsetByOwnership(FALSE)
+
+subsetByOwnership <- function(arg=TRUE){
+  if(arg==TRUE){
+    weighHouseholds("renters", "income")
+    weighHouseholds("renters", "consumption")
+    weighHouseholds("owners", "income")
+    weighHouseholds("owners", "consumption")
+        return("created subsets owners/renters for all years")
+  }else{
+        return("not implemented, error: aorns9q4o")
+  }
+}
