c43c4ccb6ddda6ea8ffa13cb14d8b156cbb86f17
academikuser <sample@.school.ac>
2017-07-23 21:32:53 +0300

    function to add size column

---
+++ R/maxReproducible.R
@@ -0,0 +1,21 @@
+#' Work with saved data or from raw census downloads? Defaults to saved. 1 for raw. 2 for saved. Takes one argument.
+#' @keywords import, saved, reproducibility
+#' @export maxReproducible
+#' @examples
+#' importData(1)
+#' importData(2)
+#' importData()
+maxReproducible <- function(level = 2){
+  if(level == 1){
+  importFamilyData("raw")
+  importFamilyData("writeOut")
+return("added $size column to familyList[[1 to 13]]")
+  }else if(level == 2){
+importData("familyProcessed")
+importData("savedIND")
+importData("saved")
+return("imported data from saved files")
+    }else{   ##"the only options are Y and N"
+        return("the only options are 1 for 'raw' and 2 for 'saved'")
+    }}
+
