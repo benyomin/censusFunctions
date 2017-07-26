3ad8d37bc94350bb0b7b7dd0e11e6603a5ea6c88
academikuser <sample@.school.ac>
2017-07-26 11:23:56 +0300

    write out munged df

--- R/maxReproducible.R
+++ R/maxReproducible.R
@@ -1,4 +1,4 @@
-#' Work with saved data or from raw census downloads?  v: 0.8
+#' Work with saved data or from raw census downloads?  v: 1.2
 #' Defaults to saved. 1 for raw. 2 for saved. 3 to import more saved.  4. to write out what was made by two.
 #' @keywords import, saved, reproducibility
 #' @export maxReproducible
@@ -8,6 +8,7 @@
 #' maxReproducible(3)
 #' maxReproducible(4)
 #' maxReproducible()
+
 maxReproducible <- function(level = 2){
   if(level == 1){
   importFamilyData("raw")
@@ -30,10 +31,10 @@ familiesList <<- dget("../dataframes/XFI_correctSize.txt")
 
 return("imported combined columns o9on99lo")
 
-  }else if(level ==4){
+  }else if(level == 4){
     ## this creates the data imported by maxReproducible(3)
-dput(familiesList, "../dataframes/XFI_correctSize.txt")
-
+          dput(familiesList, "../dataframes/XFI_correctSize.txt")
+return("saved the $size column onto familiesList")
   }else{
    return("the only options are 1:4")
   }
